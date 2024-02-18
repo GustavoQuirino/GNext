9923119810f5e5ff099add58fa3060c2dc8ab0a4
GustavoQuirino <gh.quirino@yahoo.com>
2024-02-18 11:51:09 -0300

    refactoring array de videos

--- src/app/components/tutoriais/tutoriais.component.ts
+++ src/app/components/tutoriais/tutoriais.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-tutoriais',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./tutoriais.component.css']
 })
 export class TutoriaisComponent {
+
+  videos: Array<any> = [
+    { title: 'Instalar SmartOne Samsung Antiga', url:'https://www.youtube.com/embed/s4qrovcgRyQ'},
+    { title: 'Instalar Aplicativo Downloader FireStick', url:'https://www.youtube.com/embed/DPnAeayun-w'}
+  ]
+
+  constructor( private sanitizer: DomSanitizer){ }
+
+  getSafeUrl(url: string): SafeResourceUrl {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url) 
+  }
 
 }
